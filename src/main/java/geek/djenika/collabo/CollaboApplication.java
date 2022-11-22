0c4c7ec27872fc4fb90ce14eaf48ce71cc7cb346
Aboubacar DJENIKA <75429330+geekdjenika@users.noreply.github.com>
2022-11-22 15:46:22 +0000

    Importation du projet

---
+++ src/main/java/geek/djenika/collabo/CollaboApplication.java
@@ -0,0 +1,13 @@
+package geek.djenika.collabo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/geek/djenika/collabo/CollaboApplicationTests.java
@@ -0,0 +1,13 @@
+package geek.djenika.collabo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
