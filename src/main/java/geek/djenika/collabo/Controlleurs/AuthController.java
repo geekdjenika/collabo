7bf83869b10dc50ee044e14ff47fe5fbbe60ec5b
Aboubacar DJENIKA <75429330+geekdjenika@users.noreply.github.com>
2022-11-24 21:50:16 +0000

    Implémentation l'OAuth Facebook

---
+++ src/main/java/geek/djenika/collabo/Controlleurs/AuthController.java
@@ -0,0 +1,17 @@
+package geek.djenika.collabo.Controlleurs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+}
---
+++ src/main/java/geek/djenika/collabo/Modeles/Utilisateur.java
@@ -0,0 +1,27 @@
+package geek.djenika.collabo.Modeles;
+
+import javax.persistence.*;
+/*@Entity
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")
+        })*/
+public class Utilisateur {
+        /*@Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @NotBlank
+        @Size(max = 20)
+        private String username;
+
+        @NotBlank
+        @Size(max = 50)
+        @Email
+        private String email;
+
+        @NotBlank
+        @Size(max = 120)
+        private String password;*/
+}
