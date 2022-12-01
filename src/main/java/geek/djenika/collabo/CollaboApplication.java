f67f40dd5c21494ca7c229c93f2a9b4a760a613f
Aboubacar DJENIKA <75429330+geekdjenika@users.noreply.github.com>
2022-12-01 08:23:35 +0000

    Reprise !

--- src/main/java/geek/djenika/collabo/CollaboApplication.java
+++ src/main/java/geek/djenika/collabo/CollaboApplication.java
@@ -1,10 +1,8 @@
 package geek.djenika.collabo;
 
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CollaboApplication {
--- src/main/java/geek/djenika/collabo/Controlleurs/UserController.java
+++
@@ -1,9 +0,0 @@
-package geek.djenika.collabo.Controlleurs;
-
-
-
-public class UserController {
-
-
-
-}
